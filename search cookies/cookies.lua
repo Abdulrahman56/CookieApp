d15da91e97807f0995dbd2a74fcf7f5b41db4633
Abdulrahman56 <alahmadi12349@gmail.com>
2018-07-01 19:31:55 +0300

    adding new files

---
+++ search cookies/cookies.lua
@@ -0,0 +1,66 @@
+local cookies = {
+	{
+		name = "Rich Butter",
+		thumbnail = "images/cookies/biscuits_thumb.png",
+		image = "images/cookies/biscuits.png",
+		description = "Preheat oven to 350 degrees F (175 degrees C). Grease and flour a baking sheet. -In a large bowl, stir together flour, sugar, baking powder, salt, almonds, and orange zest. Make a well in the center and add the eggs oil, and almond extract. Stir or mix by hand until the mixture forms a ball. -Separate dough into 2 pieces and roll each one into a log about 8 inches long. Place logs on prepared baking sheet and flatten so they are about 3/4 inch thick."
+	},{
+		name = "Free Nanaimo Pars",
+		thumbnail = "images/cookies/nanaimo_thumb.png",
+		image = "images/cookies/nanaimo.png",
+		description = "1/2 cup (115 g) (4 ounces) Unsalted Butter. -2 tablespoons and 2 teaspoons (40 mL) Heavy Cream. -2 tablespoons (30 mL) Vanilla Custard Powder (Such as Bird’s. Vanilla pudding mix may be substituted.) -2 cups (254 g) (8.9 ounces) Icing Sugar"
+	},{
+		name = "French Macarons",
+		thumbnail = "images/cookies/macarons_thumb.png",
+		image = "images/cookies/macarons.png",
+		description = "Preheat oven to 350 degrees F (175 degrees C). -Line a baking sheet with parchment paper. -Blend coconut, sugar, flour, vanilla extract, almond extract, and salt in a food processor until combined, about 30 seconds. -Beat egg whites in a bowl until soft peaks form. -Fold coconut mixture into egg whites until just combined. -Wet your hands. Roll spoonfuls of coconut mixture between palms to golf ball-size cookies; arrange on the prepared baking sheet."
+	},{
+		name = "Cookies Cake",
+		thumbnail = "images/cookies/cake_thumb.png",
+		image = "images/cookies/cake.png",
+		description = "Preheat oven to 350 degrees F (175 degrees C). -Pour the cake mix into a large bowl. Stir in the butter, eggs and vanilla with an electric mixer until well blended. Stir in the chocolate chips. Drop by rounded spoonfuls onto cookie sheets. -Bake for 11 to 15 minutes in the preheated oven, until the edges are golden. Cool on baking sheets for a few minutes before removing to cool on wire racks."
+	},{
+		name = "Snickerdoodle",
+		thumbnail = "images/cookies/snickerdoodle_thumb.png",
+		image = "images/cookies/snickerdoodle.png",
+		description = "Preheat the oven to 400 degrees F (205 degrees C). -Mix the shortening , sugar, and eggs until creamy. Mix the flour, cream of tartar, baking soda and salt in another bowl. Blend the wet with the dry ingredients using a wooden spoon until well-blended. -Roll the dough into the size of small walnuts. -Prepare the cinnamon and sugar mixture by mixing the cinnamon and sugar."
+	},{
+		name = "Sugar",
+		thumbnail = "images/cookies/sugar_thumb.png",
+		image = "images/cookies/sugar.png",
+		description = "Preheat oven to 375 degrees F (190 degrees C). Lightly grease baking sheets. -Cream butter or margarine, shortening, white sugar and confectioners' sugar together until light and fluffy. Beat in the egg and the vanilla. -Sift the flour, baking soda, salt and cream of tartar together. Add the flour mixture to the sugar mixture and blend. -Shape dough into walnut-sized balls and place on the prepared baking sheets. Dip a glass in white sugar and press balls flat."
+	},{
+		name = "Oatmeal Raisin",
+		thumbnail = "images/cookies/oatmeal_thumb.png",
+		image = "images/cookies/oatmeal.png",
+		description = "In a large bowl, cream together the shortening, white sugar and brown sugar until smooth. Beat in the eggs one at a time, then stir in the orange zest, molasses and vanilla. Combine the flour, baking soda and salt; stir into the molasses mixture until well blended. Mix in oats. -Divide the dough into halves, and shape into long logs about 2 inches in diameter. Wrap in waxed paper, and refrigerate for at least 8 hours or overnight."
+	},{
+		name = "Polvorones Con Canela",
+		thumbnail = "images/cookies/polvorones_thumb.png",
+		image = "images/cookies/polvorones.png",
+		description = "1 stick unsalted butter, softened. -1 cup Powdered sugar,sifted. -1/2 tbsp vanilla extract. -1 cup all purpose flour. -pinch of salt. -1/2 cup ground pecans. -1/2 tsp ground cinnamon"
+	},{
+		name = "Peanut Butter",
+		thumbnail = "images/cookies/peanut_thumb.png",
+		image = "images/cookies/peanut.png",
+		description = "Preheat oven to 350 degrees F (175 degrees C). Grease cookie sheets. -In medium bowl, cream together the shortening, sugar and peanut butter. Beat in the egg and vanilla. Combine the flour, baking soda and salt, stir into the creamed mixture until well blended. Roll the dough into walnut sized balls. Place the cookies 2 inches apart onto the cookie sheets. Press balls down using a fork dipped in sugar water. -Bake for 8 to 10 minutes in the preheated oven."
+	},{
+		name = "Fig Newtons",
+		thumbnail = "images/cookies/fig_thumb.png",
+		image = "images/cookies/fig.png",
+		description = "Heat oven to 350°F. Spray 13x9-inch pan with cooking spray. In large bowl, place cookie mix. Cut in butter, using pastry blender (or pulling 2 table knives through mix in opposite directions), until mixture looks like coarse crumbs. Reserve 1 1/2 cups of the crumb mixture for topping. Press remaining mixture in bottom of pan. Bake 10 minutes."
+	},{
+		name = "Ginger",
+		thumbnail = "images/cookies/ginger_thumb.png",
+		image = "images/cookies/ginger.png",
+		description = "Preheat oven to 350 degrees F (175 degrees C). Grease cookie sheets. -In a large bowl, cream the shortening and sugar. Add the egg and beat until light and fluffy, then stir in the molasses. In a separate bowl, mix together the flour, baking soda, salt, ginger and cinnamon. Add to the egg mixture and stir until well blended. Roll bits of dough into 1 inch balls. Dip each ball in sugar and place on cookie sheet, sugared side up about 2 inches apart. -Bake for 10 to 12 minutes."
+	},{
+		name = "Dill and Botato",
+		thumbnail = "images/cookies/potato_thumb.png",
+		image = "images/cookies/potato.png",
+		description = "2 cups self-rising flour. -3 tbsp butter, softened. -pinch of salt. -1 tbsp finely chopped fresh dill. -1 cup mashed potatoes (I steamed about 2 potatoes and mashed them). -2-3 tbsp milk, as required"
+	}
+}
+
+return cookies
+	
